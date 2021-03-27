dafbd46f793ea7926296ceefbabd0a05816c576f
Corentin Royer <corentin.35035@gmail.com>
2021-03-27 23:22:52 +0100

    Finished transfering preview to backend

--- mailchimp-backend/functions/index.js
+++ mailchimp-backend/functions/index.js
@@ -15,8 +15,8 @@ mailchimp.setConfig({
 })
 
 //Some constant
-// const testEmails = ["responsable.commercial@telecom-etude.fr", "secretaire.general@telecom-etude.fr"]
-const testEmails = ["hugo.queinnec@telecom-etude.fr", "corentin.royer@telecom-etude.fr"]
+const testEmails = ["responsable.commercial@telecom-etude.fr", "secretaire.general@telecom-etude.fr"]
+//const testEmails = ["hugo.queinnec@telecom-etude.fr", "corentin.royer@telecom-etude.fr"]
 const setHTMLContentBool = true
 const sentTestEmailsBool = true
 
@@ -27,47 +27,16 @@ exports.createCampaignAndSendTestEmail = functions.https.onCall(async (data, con
 		// Throwing an HttpsError so that the client gets the error details.
 		throw new functions.https.HttpsError("unauthenticated", "The function must be called while authenticated.")
 	}
-
 	//Format the data for the next function
-	const [
-		contentTitle,
-		contentFirstDescription,
-		contentDomain,
-		imageDomainLink,
-		contentPay,
-		imagePayLink,
-		contentDifficulty,
-		imageDifficultyLink,
-		contentSkills,
-		contentSchedule,
-		contentDescription,
-		contentApply,
-		contentMailContact,
-		contentMailTo,
-	] = await checkAndFormatData(data)
+	data = await checkAndFormatData(data)
 
 	//Generate the html content
-	const htmlContent = await contentEditHTML(
-		contentTitle,
-		contentFirstDescription,
-		contentDomain,
-		imageDomainLink,
-		contentPay,
-		imagePayLink,
-		contentDifficulty,
-		imageDifficultyLink,
-		contentSkills,
-		contentSchedule,
-		contentDescription,
-		contentApply,
-		contentMailContact,
-		contentMailTo
-	)
+	const htmlContent = await contentEditHTML(data)
 
-	const campaignName = contentTitle
-	const mailObject = "[Telecom Etude] " + contentTitle
+	const campaignName = data.contentTitle
+	const mailObject = "[Telecom Etude] " + data.contentTitle
 	const mailFromName = "Telecom Etude"
-	const mailReplyTo = contactList[0] //Or set it to candidature@telecom-etude.fr ?
+	const mailReplyTo = data.contactList[0] //Or set it to candidature@telecom-etude.fr ?
 
 	console.log("Creating the campaing...")
 
@@ -97,7 +66,7 @@ exports.createCampaignAndSendTestEmail = functions.https.onCall(async (data, con
 		}
 	}
 
-	return "Your campaign has been created and has been sent to the auditors"
+	return "Your campaign has been created and has been sent to the auditors."
 })
 
 //The function to get the preview email
@@ -109,40 +78,10 @@ exports.getPreviewEmail = functions.https.onCall(async (data, context) => {
 	}
 
 	//Format the data for the next function
-	const [
-		contentTitle,
-		contentFirstDescription,
-		contentDomain,
-		imageDomainLink,
-		contentPay,
-		imagePayLink,
-		contentDifficulty,
-		imageDifficultyLink,
-		contentSkills,
-		contentSchedule,
-		contentDescription,
-		contentApply,
-		contentMailContact,
-		contentMailTo,
-	] = await checkAndFormatData(data)
+	data = await checkAndFormatData(data)
 
 	//Generate the html content
-	const htmlContent = await contentEditHTML(
-		contentTitle,
-		contentFirstDescription,
-		contentDomain,
-		imageDomainLink,
-		contentPay,
-		imagePayLink,
-		contentDifficulty,
-		imageDifficultyLink,
-		contentSkills,
-		contentSchedule,
-		contentDescription,
-		contentApply,
-		contentMailContact,
-		contentMailTo
-	)
+	const htmlContent = await contentEditHTML(data)
 
 	return htmlContent
 })
@@ -174,83 +113,35 @@ async function checkAndFormatData(data) {
 		throw new functions.https.HttpsError("invalid-argument", "Not all fields are filled.")
 	}
 
-	//Raw data
-	contentTitle = data.contentTitle
-	contentFirstDescription = data.contentFirstDescription
-	contentDomain = data.contentDomain
-	contentPay = data.contentPay
-	contentDifficulty = data.contentDifficulty
-	contentSkills = data.contentSkills
-	contentSchedule = data.contentSchedule
-	contentDescription = data.contentDescription
+	data = await contentTransformations(data)
 
-	//Data to transform
-	imageDomain = data.imageDomain
-	imagePay = data.imagePay
-	imageDifficulty = data.imageDifficulty
-	formBoolean = data.formBoolean
-	formLink = data.formLink
-
-	contactList = data.contactList
-
-	const [
-		imageDomainLink,
-		imagePayLink,
-		imageDifficultyLink,
-		contentApply,
-		contentMailContact,
-		contentMailTo,
-	] = await contentTransformations(imageDomain, imagePay, imageDifficulty, formBoolean, formLink, contactList)
-
-	return [
-		contentTitle,
-		contentFirstDescription,
-		contentDomain,
-		imageDomainLink,
-		contentPay,
-		imagePayLink,
-		contentDifficulty,
-		imageDifficultyLink,
-		contentSkills,
-		contentSchedule,
-		contentDescription,
-		contentApply,
-		contentMailContact,
-		contentMailTo,
-	]
+	return data
 }
 
-async function contentTransformations(imageDomain, imagePay, imageDifficulty, formBoolean, formLink, contactList) {
-	var imageDomainLink = ""
-	var imagePayLink = ""
-	var imageDifficultyLink = ""
-	var contentApply = ""
-	var contentMailContact = ""
-	var contentMailTo = ""
-
-	switch (imageDomain) {
+async function contentTransformations(data) {
+	switch (data.imageDomain) {
 		case "data":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/3f084638-6053-45c6-a99e-7ad481f340fc.png"
 			break
 		case "dev":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/458e3a19-7fe7-4471-aad4-8d19e43f2383.png"
 			break
 		case "cyber":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/4d65cc38-f99c-4eb5-bd8b-f4c58d44aecb.png"
 			break
 		case "se":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/ad81493a-5281-4772-adef-4bcebee00243.png"
 			break
 		case "image":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/863635c4-6fc0-42d0-add6-7c4b8aa04b9b.png"
 			break
 		case "etude":
-			imageDomainLink =
+			data.imageDomainLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/0104e278-2e89-44d0-823f-86e2e92a1e70.png"
 			break
 		default:
@@ -258,17 +149,17 @@ async function contentTransformations(imageDomain, imagePay, imageDifficulty, fo
 			throw new functions.https.HttpsError("invalid-argument", "The domain image name is incorrect.")
 	}
 
-	switch (imagePay) {
+	switch (data.imagePay) {
 		case "low":
-			imagePayLink =
+			data.imagePayLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/aa0aa2c2-9aaf-46f8-ae91-8c28953b02bc.png"
 			break
 		case "middle":
-			imagePayLink =
+			data.imagePayLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/d790eeff-772d-4aa7-91a0-f1883414b675.png"
 			break
 		case "high":
-			imagePayLink =
+			data.imagePayLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/7781ba0c-a348-4722-ba77-beee6bbb5f51.png"
 			break
 		default:
@@ -276,17 +167,17 @@ async function contentTransformations(imageDomain, imagePay, imageDifficulty, fo
 			throw new functions.https.HttpsError("invalid-argument", "The pay image name is incorrect.")
 	}
 
-	switch (imageDifficulty) {
+	switch (data.imageDifficulty) {
 		case "low":
-			imageDifficultyLink =
+			data.imageDifficultyLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/b30e558d-f0e5-489a-80ba-681d27021c0a.png"
 			break
 		case "middle":
-			imageDifficultyLink =
+			data.imageDifficultyLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/1f020378-3a07-4a5b-892c-62fd7822e9f0.png"
 			break
 		case "high":
-			imageDifficultyLink =
+			data.imageDifficultyLink =
 				"https://mcusercontent.com/d64b9431d63c83512b8b612ee/images/93539be4-2afc-4c5f-a34e-f09fa0daa6b1.png"
 			break
 		default:
@@ -294,79 +185,65 @@ async function contentTransformations(imageDomain, imagePay, imageDifficulty, fo
 			throw new functions.https.HttpsError("invalid-argument", "The difficulty image name is incorrect.")
 	}
 
-    if (formBoolean) {
-        const templates_ref = db.collection("global_const").doc("templates")
-        const templates = await templates_ref.get()
-        
-        contentApply = templates.data()["templateForm"]
-        contentApply = contentApply.replace("FORM_LINK", formLink)
+	if (data.formBoolean) {
+		const templates_ref = db.collection("global_const").doc("templates")
+		const templates = await templates_ref.get()
 
-    } else {
-        const templates_ref = db.collection("global_const").doc("templates")
-        const templates = await templates_ref.get()
-        
-        contentApply = templates.data()["templateNoForm"]
+		data.contentApply = templates.data()["templateForm"]
+		data.contentApply = data.contentApply.replace("FORM_LINK", data.formLink)
+	} else {
+		const templates_ref = db.collection("global_const").doc("templates")
+		const templates = await templates_ref.get()
+
+		data.contentApply = templates.data()["templateNoForm"]
 	}
 
 	contentMailTo = "mailto:"
-	for (var i = 0; i < contactList.length; i++) {
-		var mail = contactList[i]
+	for (var i = 0; i < data.contactList.length; i++) {
+		var mail = data.contactList[i]
 		contentMailTo += mail + ","
 	}
 	contentMailTo += "?subject=Envoi%20du%20CV%20pour%20l'%C3%A9tude%20"
+	data.contentMailTo = contentMailTo
 
 	contentMailContact = "N'hésitez pas à demander plus d'informations ou de détails à "
-	for (var i = 0; i < contactList.length; i++) {
-		var mail = contactList[i]
+	for (var i = 0; i < data.contactList.length; i++) {
+		var mail = data.contactList[i]
 		var name = mail.charAt(0).toUpperCase() + mail.substring(1, mail.indexOf("."))
-		if (contactList.length == 1) {
+		if (data.contactList.length == 1) {
 			contentMailContact += name + " (" + mail + ")."
-		} else if (i != contactList.length - 1) {
+		} else if (i != data.contactList.length - 1) {
 			contentMailContact += name + " (" + mail + "), "
 		} else {
 			contentMailContact += "et " + name + " (" + mail + ")."
 		}
 	}
+	data.contentMailContact = contentMailContact
 
-	return [imageDomainLink, imagePayLink, imageDifficultyLink, contentApply, contentMailContact, contentMailTo]
+	return data
 }
 
-async function contentEditHTML(
-	contentTitle,
-	contentFirstDescription,
-	contentDomain,
-	imageDomainLink,
-	contentPay,
-	imagePayLink,
-	contentDifficulty,
-	imageDifficultyLink,
-	contentSkills,
-	contentSchedule,
-	contentDescription,
-	contentApply,
-	contentMailContact,
-	contentMailTo
-) {
+async function contentEditHTML(data) {
 	const templates_ref = db.collection("global_const").doc("templates")
-    const templates = await templates_ref.get()
-    
-    htmlContent = templates.data()["templateBody"].toString()
-    console.log(typeof htmlContent)
+	const templates = await templates_ref.get()
 
-	htmlContent = htmlContent.replace("CONTENT_TITLE", contentTitle)
-	htmlContent = htmlContent.replace("CONTENT_FIRST_DESCRIPTION", contentFirstDescription)
-	htmlContent = htmlContent.replace("CONTENT_DOMAIN", contentDomain)
-	htmlContent = htmlContent.replace("IMAGE_DOMAIN_LINK", imageDomainLink)
-	htmlContent = htmlContent.replace("CONTENT_PAY", contentPay)
-	htmlContent = htmlContent.replace("IMAGE_PAY_LINK", imagePayLink)
-	htmlContent = htmlContent.replace("CONTENT_DIFFICULTY", contentDifficulty)
-	htmlContent = htmlContent.replace("IMAGE_DIFFICULTY_LINK", imageDifficultyLink)
-	htmlContent = htmlContent.replace("CONTENT_SKILLS", contentSkills)
-	htmlContent = htmlContent.replace("CONTENT_SCHEDULE", contentSchedule)
-	htmlContent = htmlContent.replace("CONTENT_DESCRIPTION", contentDescription)
-	htmlContent = htmlContent.replace("CONTENT_APPLY", contentApply)
-	htmlContent = htmlContent.replace("CONTENT_MAIL_CONTACT", contentMailContact)
-	htmlContent = htmlContent.replace("CONTENT_MAILTO", contentMailTo)
+	htmlContent = templates.data()["templateBody"].toString()
+	console.log(typeof htmlContent)
+
+	htmlContent = htmlContent.replace("CONTENT_TITLE", data.contentTitle)
+	htmlContent = htmlContent.replace("CONTENT_FIRST_DESCRIPTION", data.contentFirstDescription)
+	htmlContent = htmlContent.replace("CONTENT_DOMAIN", data.contentDomain)
+	htmlContent = htmlContent.replace("IMAGE_DOMAIN_LINK", data.imageDomainLink)
+	htmlContent = htmlContent.replace("CONTENT_PAY", data.contentPay)
+	htmlContent = htmlContent.replace("IMAGE_PAY_LINK", data.imagePayLink)
+	htmlContent = htmlContent.replace("CONTENT_DIFFICULTY", data.contentDifficulty)
+	htmlContent = htmlContent.replace("IMAGE_DIFFICULTY_LINK", data.imageDifficultyLink)
+	htmlContent = htmlContent.replace("CONTENT_SKILLS", data.contentSkills)
+	htmlContent = htmlContent.replace("CONTENT_SCHEDULE", data.contentSchedule)
+	htmlContent = htmlContent.replace("CONTENT_DESCRIPTION", data.contentDescription)
+	htmlContent = htmlContent.replace("CONTENT_APPLY", data.contentApply)
+	htmlContent = htmlContent.replace("CONTENT_MAIL_CONTACT", data.contentMailContact)
+	htmlContent = htmlContent.replace("CONTENT_MAILTO", data.contentMailTo)
 
 	return htmlContent
 }
